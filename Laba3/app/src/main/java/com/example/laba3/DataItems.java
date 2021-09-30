e2aa048201eb666f670e56da3e148b92c2c9c7d6
Сергей Резников <tikto01@mail.ru>
2021-09-30 20:29:35 +0400

    ready

---
+++ Laba3/app/src/main/java/com/example/laba3/DataItems.java
@@ -0,0 +1,13 @@
+package com.example.laba3;
+import java.util.List;
+
+public class DataItems {
+    private List<Student> users;
+
+    public List<Student> getUsers() {
+        return users;
+    }
+    public void setUsers(List<Student> users) {
+        this.users = users;
+    }
+}
